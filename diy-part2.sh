d5b100852c0a3186f148a0ffe2d8a4db2660b31c
huawuhen <lovewuhen@gmail.com>
2023-02-24 21:43:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# zidingyi chajian
+git clone https://github.com/sbwml/luci-app-alist package/lean/luci-app-alist

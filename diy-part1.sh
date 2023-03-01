179618c81b4cf5c106429f47af260048da930772
huawuhen <lovewuhen@gmail.com>
2023-03-01 15:57:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# zidingyi chajian
+git clone https://github.com/sbwml/luci-app-alist package/lean/alist

67cd5c42c1157e3c111bb3fbbe0485cf1bcedac5
huawuhen <lovewuhen@gmail.com>
2023-03-01 13:54:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,4 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # zidingyi chajian
-git clone https://github.com/sbwml/luci-app-alist package/alist
+git clone https://github.com/sbwml/luci-app-alist package/luci/alist
